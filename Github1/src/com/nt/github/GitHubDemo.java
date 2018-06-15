03103f48cdef455eca1c62a33f96382c65f18002
YUGSHUKLA <YUGSHUKLA@192.168.0.13>
2018-06-16 00:35:20 +0530

    i'm ,Add m1 method

--- Github1/src/com/nt/github/GitHubDemo.java
+++ Github1/src/com/nt/github/GitHubDemo.java
@@ -2,9 +2,17 @@ package com.nt.github;
 
 public class GitHubDemo {
 
+	public static int m1(){
+		System.out.println("m1 method");
+		return 20;
+	}
+		
+	
+	
 	public static void main(String[] args) {
 		System.out.println("hello");
-
+		
+System.out.println(GitHubDemo.m1());
 	}
 
 }
